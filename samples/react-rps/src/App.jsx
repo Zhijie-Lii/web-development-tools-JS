6527a2d8b02c6f15c672b350c94b3d45077e7c71
Brett Ritter <b.ritter@neu.edu>
2021-03-28 15:14:05 -0700

    Adds Mar 26 Notes, samples, react-rest assignment

---
+++ samples/react-rps/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Play from './Play';
+import Results from './Results';
+
+function App() {
+
+  return (
+    <div className="app">
+      <Results/>
+      <Play/>
+    </div>
+  );
+}
+
+export default App;
